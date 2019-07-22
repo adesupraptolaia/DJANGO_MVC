2d5a742d0ceb400054a438bb624d5ef73fa7c05a
adesupraptolaia <ades@alterra.id>
2019-07-22 11:27:11 +0700

    challenge django 1

---
+++ project_advance_views/env_project_advance-views/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta16/Desktop/Tugas Django/project_advance_views/env_project_advance-views/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
